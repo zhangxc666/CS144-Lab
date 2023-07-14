1cdd720ed235db64b8cadd602eb74b29650ab691
zxc <1315730237@qq.com>
2023-07-14 19:23:14 +0800

    完成Lab1 Reassembler 0.77Gb/s

--- src/reassembler.cc
+++ src/reassembler.cc
@@ -2,17 +2,71 @@
 
 using namespace std;
 
-void Reassembler::insert( uint64_t first_index, string data, bool is_last_substring, Writer& output )
-{
-  // Your code here.
-  (void)first_index;
-  (void)data;
-  (void)is_last_substring;
-  (void)output;
+void Reassembler::insert(uint64_t first_index, string data, bool is_last_substring, Writer &output) {
+    if (is_last_substring)isFinish = true;
+    if (data.size() == 0) {
+        if (isFinish and totalBytes == 0)output.close();
+        return;
+    }
+    uint64_t available_size = output.available_capacity(); // 剩余容量
+    uint64_t end = pushtotalBytes - 1 + available_size; // 最后一个可容纳的最后一个字节索引
+    uint64_t st = pushtotalBytes;       // 第一个可容纳的字节索引
+    uint64_t l = first_index;
+    uint64_t r = first_index + data.size() - 1;
+    // [l,r]和[st,end]必须要有重叠
+    if (l > end || r < st) {
+        if (isFinish and totalBytes == 0)output.close();
+        return;
+    }
+    if (l < st) {  // 若当前一部分数据已经被取走过了
+        data = data.substr(st - l);
+        l = st;
+    }
+    if (r > end) {    // 有一部分数据超出当前容量，截断
+        r = end;
+        data = data.substr(0, r - l + 1);
+    }
+    uint64_t lmin = l, rmax = r;
+    for (auto &[ll, rr]: s) { // 遍历区间
+        if ((ll <= rmax and rr >= rmax) or (rr >= lmin and ll <= lmin)) { //两个区间有重合
+            lmin = min(lmin, ll);
+            rmax = max(rmax, rr);
+        }else if(ll>rmax)break;
+    }
+    string temp_s(rmax - lmin + 1, '0');
+    uint64_t sum = rmax - lmin + 1;
+    set<PII> temp = s;
+    for (auto &[ll, rr]: temp) {
+        if (ll >= lmin and rr <= rmax) {
+            s.erase({ll, rr});
+            mergeString(lmin, ll, rr, temp_s, mp[ll]);
+            sum -= (rr - ll + 1);
+            mp.erase(ll);
+        }
+    }
+    s.insert({lmin, rmax});
+    mergeString(lmin, l, r, temp_s, data);
+    totalBytes += sum;
+    mp[lmin] = temp_s;
+    while (s.size() and (s.begin()->first == pushtotalBytes)) {
+        st = pushtotalBytes;
+        output.push(mp[st]);
+        s.erase(s.begin());
+        totalBytes -= mp[st].size();
+        pushtotalBytes += mp[st].size();
+        mp.erase(st);
+    }
+    if (isFinish and totalBytes == 0)output.close();
+    return;
 }
 
-uint64_t Reassembler::bytes_pending() const
-{
-  // Your code here.
-  return {};
+uint64_t Reassembler::bytes_pending() const {
+    // Your code here.
+    return totalBytes;
+}
+
+void Reassembler::mergeString(int lmin, int l, int r, std::string &dest, std::string &src) {
+    for (int i = (l - lmin), j = 0; i <= (r - lmin); i++, j++) {
+        dest[i] = src[j];
+    }
 }
--- src/reassembler.hh
+++ src/reassembler.hh
@@ -3,32 +3,43 @@
 #include "byte_stream.hh"
 
 #include <string>
+#include <set>
+#include <map>
+using namespace std;
+typedef pair<uint64_t , uint64_t> PII;
 
-class Reassembler
-{
+class Reassembler {
+private:
+    bool isFinish;
+    uint64_t pushtotalBytes; // 当前已经推进去的字节数
+    uint64_t totalBytes;     // 当前存储的字节数
+    set<PII> s;// 维护一个区间
+    map<uint64_t,string> mp; // 当前下标对应的字符串
+    void mergeString(int lmin,int l,int r,string &dest,string &src);
 public:
-  /*
-   * Insert a new substring to be reassembled into a ByteStream.
-   *   `first_index`: the index of the first byte of the substring
-   *   `data`: the substring itself
-   *   `is_last_substring`: this substring represents the end of the stream
-   *   `output`: a mutable reference to the Writer
-   *
-   * The Reassembler's job is to reassemble the indexed substrings (possibly out-of-order
-   * and possibly overlapping) back into the original ByteStream. As soon as the Reassembler
-   * learns the next byte in the stream, it should write it to the output.
-   *
-   * If the Reassembler learns about bytes that fit within the stream's available capacity
-   * but can't yet be written (because earlier bytes remain unknown), it should store them
-   * internally until the gaps are filled in.
-   *
-   * The Reassembler should discard any bytes that lie beyond the stream's available capacity
-   * (i.e., bytes that couldn't be written even if earlier gaps get filled in).
-   *
-   * The Reassembler should close the stream after writing the last byte.
-   */
-  void insert( uint64_t first_index, std::string data, bool is_last_substring, Writer& output );
+    Reassembler():isFinish(false),pushtotalBytes(0),totalBytes(0),s(),mp(){}
+    /*
+     * Insert a new substring to be reassembled into a ByteStream.
+     *   `first_index`: the index of the first byte of the substring
+     *   `data`: the substring itself
+     *   `is_last_substring`: this substring represents the end of the stream
+     *   `output`: a mutable reference to the Writer
+     *
+     * The Reassembler's job is to reassemble the indexed substrings (possibly out-of-order
+     * and possibly overlapping) back into the original ByteStream. As soon as the Reassembler
+     * learns the next byte in the stream, it should write it to the output.
+     *
+     * If the Reassembler learns about bytes that fit within the stream's available capacity
+     * but can't yet be written (because earlier bytes remain unknown), it should store them
+     * internally until the gaps are filled in.
+     *
+     * The Reassembler should discard any bytes that lie beyond the stream's available capacity
+     * (i.e., bytes that couldn't be written even if earlier gaps get filled in).
+     *
+     * The Reassembler should close the stream after writing the last byte.
+     */
+    void insert(uint64_t first_index, std::string data, bool is_last_substring, Writer &output);
 
-  // How many bytes are stored in the Reassembler itself?
-  uint64_t bytes_pending() const;
+    // How many bytes are stored in the Reassembler itself?
+    uint64_t bytes_pending() const;
 };
